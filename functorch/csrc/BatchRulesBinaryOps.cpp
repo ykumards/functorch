92fff94ff383728d1b2558bb7df8f06caaa6f410
samdow <samdow@fb.com>
2022-03-21 10:50:40 -0700

    prelu forward rule

--- functorch/csrc/BatchRulesBinaryOps.cpp
+++ functorch/csrc/BatchRulesBinaryOps.cpp
@@ -275,6 +275,60 @@ std::tuple<Tensor,optional<int64_t>> cdist_backward_batch_rule(
   return std::make_tuple(out, out_bdim);
 }
 
+std::tuple<Tensor,optional<int64_t>> prelu_batch_rule(
+    const Tensor& input, optional<int64_t> input_bdim,
+    const Tensor& weight, optional<int64_t> weight_bdim) {
+  if (!weight_bdim && weight.dim() == 0) {
+    return std::make_tuple(at::prelu(input, weight), input_bdim);
+  }
+
+  const auto input_ = moveBatchDimToFront(input, input_bdim);
+  auto weight_flatten = moveBatchDimToFront(weight, weight_bdim);
+  if (weight_flatten.dim() > 1) {
+    // for an input [N, C, ...]
+    // weight can be a non-vector but the total number of elements must be the same as C
+    weight_flatten = at::flatten(weight_flatten, weight_bdim.has_value() ? 1 : 0, -1);
+  }
+
+  const int64_t input_logical_rank = rankWithoutBatchDim(input, input_bdim);
+  VmapDimVector new_shape(weight_flatten.sizes().begin(), weight_flatten.sizes().end());
+  const int64_t final_size = weight_bdim ? (input_logical_rank + 1) : input_logical_rank;
+  new_shape.reserve(final_size);
+
+  if (weight_flatten.dim() == 2 || !weight_bdim) {
+    // copies checks from prelu if the weight (without vmap) is not a scalar
+    TORCH_CHECK(input_logical_rank > 0, "Not allow zero-dim input tensor.");
+
+    int64_t channel_size = 1; // channel_size default to 1
+    if (input_logical_rank > 1) {
+      const auto channel_dim = input_bdim ? 2 : 1;
+      channel_size = input_.size(channel_dim);
+    }
+    const auto weight_num = weight_flatten.size(-1);
+    TORCH_CHECK(channel_size == weight_num,
+      "Mismatch of parameter numbers and input channel size. Found parameter numbers = ", weight_num,
+      " and channel size = ", channel_size, ".");
+
+    // pads to the left so that the flattened shape matches up with the channel
+    if (!weight_bdim) {
+      new_shape.insert(new_shape.begin(), 1); 
+    } else {
+      new_shape.insert(new_shape.begin() + 1, 1);
+    }
+  }
+
+  for (int64_t i = new_shape.size(); i < final_size; i ++) {
+    new_shape.push_back(1);
+  }
+  TORCH_INTERNAL_ASSERT(new_shape.size() == final_size);
+  const auto weight_padded = weight_flatten.view(new_shape);
+  auto zero_tensor = at::zeros(1);
+
+  // decomposes function, 
+  auto res = at::maximum(zero_tensor, input_) + weight_padded * at::minimum(zero_tensor, input_);
+  return std::make_tuple(res, 0);
+}
+
 TORCH_LIBRARY_IMPL(aten, FuncTorchVmapMode, m) {
   #define BINARY_RANDOM_POINTWISE(op) \
   m.impl(#op, BINARY_RANDOM_POINTWISE_BATCH_RULE(ATEN_FN(op)));
@@ -377,6 +431,7 @@ TORCH_LIBRARY_IMPL(aten, FT_BATCHED_KEY, m) {
   BINARY_SCALAR_3(pow, Tensor_Tensor, Tensor_Scalar, Scalar);
   BINARY_POINTWISE(polar);
   POINTWISE_BOXED(polygamma);
+  VMAP_SUPPORT(prelu, prelu_batch_rule)
   BINARY_SCALAR_2(sub, Tensor, Scalar);
   BINARY_SCALAR_3(remainder, Tensor, Scalar, Scalar_Tensor);
   BINARY_POINTWISE(rrelu_with_noise);
